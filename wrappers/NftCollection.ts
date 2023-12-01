95c7465ba36110b801a4f9bdbba37aa0d7d3c585
Cosmodude <vladislav.lenskii@gmail.com>
2023-12-01 13:25:22 +0900

    wrapper created

--- wrappers/NftCollection.ts
+++ wrappers/NftCollection.ts
@@ -1,9 +1,43 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import { 
+    Address, 
+    beginCell, 
+    Cell, 
+    Contract, 
+    contractAddress, 
+    ContractProvider, 
+    Sender, 
+    SendMode,
+    TupleItemInt, 
+} from 'ton-core';
 
-export type NftCollectionConfig = {};
+
+export type RoyaltyParams = {
+    royaltyFactor: number;
+    royaltyBase: number;
+    royaltyAddress: Address;
+};
+
+export type NftCollectionConfig = {
+    ownerAddress: Address;
+    nextItemIndex: number;
+    collectionContent: Cell;
+    nftItemCode: Cell;
+    royaltyParams: RoyaltyParams;
+};
 
 export function nftCollectionConfigToCell(config: NftCollectionConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeAddress(config.ownerAddress)
+        .storeUint(config.nextItemIndex, 64)
+        .storeRef(config.collectionContent)
+        .storeRef(config.nftItemCode)
+        .storeRef(
+            beginCell()
+                .storeUint(config.royaltyParams.royaltyFactor, 16)
+                .storeUint(config.royaltyParams.royaltyBase, 16)
+                .storeAddress(config.royaltyParams.royaltyAddress)
+        )
+    .endCell();
 }
 
 export class NftCollection implements Contract {
@@ -26,4 +60,69 @@ export class NftCollection implements Contract {
             body: beginCell().endCell(),
         });
     }
+
+    async sendMintNft(provider: ContractProvider, via: Sender,
+        opts: {
+            value: bigint;
+            queryId: number;
+            amount: bigint;  // to send with nft
+            itemIndex: number;
+            itemOwnerAddress: Address;
+            itemContent: Cell;
+           
+        }
+        ) {
+            const nftMessage = beginCell();
+            nftMessage.storeAddress(opts.itemOwnerAddress)
+            nftMessage.storeRef(opts.itemContent)
+            await provider.internal(via, {
+                value: opts.value,
+                sendMode: SendMode.PAY_GAS_SEPARATELY,
+                body: beginCell()
+                    .storeUint(1,32)  // operation
+                    .storeUint(opts.queryId,64)
+                    .storeUint(opts.itemIndex,64)
+                    .storeCoins(opts.amount)
+                    .storeRef(nftMessage)  // body
+                .endCell()
+            })
+        }
+
+    async sendChangeOwner(provider: ContractProvider, via: Sender,
+        opts: {
+            value: bigint;
+            queryId: bigint;
+            newOwnerAddress: Address;
+        }
+        ) { 
+            await provider.internal(via, {
+                value: opts.value,
+                sendMode: SendMode.PAY_GAS_SEPARATELY,
+                body: beginCell()
+                    .storeUint(3,32) //operation
+                    .storeUint(opts.queryId, 64)
+                    .storeAddress(opts.newOwnerAddress)
+                .endCell()
+            })
+    }
+
+    async getCollectionData(provider: ContractProvider): Promise<{nextItemId: bigint, ownerAddress: Address, collectionContent: Cell}>{
+        const collection_data = await provider.get("get_collection_data", []);
+        const stack = await collection_data.stack;
+        let nextItem: bigint = stack.readBigNumber();
+        let collectionContent = await stack.readCell();
+        let ownerAddress = await stack.readAddress();
+        return {
+            nextItemId: nextItem, 
+            collectionContent: collectionContent,
+            ownerAddress: ownerAddress
+        };
+    }
+
+    async getItemAddressByIndex(provider: ContractProvider, index: TupleItemInt){
+        const res = await provider.get("get_nft_address_by_index", [index]);
+        const itemAddress = await res.stack.readAddress()
+        return itemAddress;
+    }
+
 }
