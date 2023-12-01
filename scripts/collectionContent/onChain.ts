b404be6e8af8363f459a3fd1e72556f12d96f899
Cosmodude <vladislav.lenskii@gmail.com>
2023-12-01 13:25:36 +0900

    script + helpers

---
+++ scripts/collectionContent/onChain.ts
@@ -0,0 +1,45 @@
+import { Dictionary, beginCell, Cell } from 'ton-core';
+import { sha256_sync } from 'ton-crypto'
+
+export function toSha256(s: string): bigint {
+    return BigInt('0x' + sha256_sync(s).toString('hex'))
+}
+
+export function toTextCell(s: string): Cell {
+    return beginCell().storeUint(0, 8).storeStringTail(s).endCell()
+}
+
+export type collectionContent = {
+    name: string,
+    description: string,
+    image: string
+}
+
+export type itemContent = {
+    name: string,
+    description: string,
+    image: string,
+}
+
+export function buildCollectionContentCell(content: collectionContent): Cell {
+    const collectionContentDict = Dictionary.empty(Dictionary.Keys.BigUint(256), Dictionary.Values.Cell())
+        .set(toSha256("name"), toTextCell(content.name))
+        .set(toSha256("description"), toTextCell(content.description))
+        .set(toSha256("image"), toTextCell(content.image));
+    
+    return beginCell() // need to fix 
+            .storeUint(0,8)
+            .storeDict(collectionContentDict)
+            .endCell(); 
+    }
+
+export function setItemContentCell(content: itemContent): Cell {
+    const itemContentDict = Dictionary.empty(Dictionary.Keys.BigUint(256), Dictionary.Values.Cell())
+        .set(toSha256("name"), toTextCell(content.name))
+        .set(toSha256("description"), toTextCell(content.description))
+        .set(toSha256("image"), toTextCell(content.image))
+    return beginCell()
+            .storeUint(0,8)
+            .storeDict(itemContentDict)
+            .endCell(); 
+}
--- scripts/deployNftCollection.ts
+++ scripts/deployNftCollection.ts
@@ -1,13 +1,52 @@
-import { toNano } from 'ton-core';
+import { Address, toNano } from 'ton-core';
 import { NftCollection } from '../wrappers/NftCollection';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
+import { buildCollectionContentCell, setItemContentCell } from './collectionContent/onChain';
+
+let myAddress: Address = Address.parse("kQAXUIBw-EDVtnCxd65Z2M21KTDr07RoBL6BYf-TBCd6dTBu");
+
+const randomSeed= Math.floor(Math.random() * 10000);
+
+const nextonAddress = Address.parse("EQBDqObEyc8KYOuHCKm0evBNp0hJ9derp8eSIdhYMjIeMRSZ");
 
 export async function run(provider: NetworkProvider) {
-    const nftCollection = provider.open(NftCollection.createFromConfig({}, await compile('NftCollection')));
+    const nftCollection = provider.open(NftCollection.createFromConfig({
+        ownerAddress: myAddress, 
+        nextItemIndex: 0,
+        collectionContent: buildCollectionContentCell({
+            name: "NexTon Liquid Derivatives Staking",
+            description: "Collection of liquidity staking derivatives, issued by NexTon",
+            image: "https://raw.githubusercontent.com/Cosmodude/Nexton/main/Nexton_Logo.jpg"
+        }),
+        //off chain
+        // collectionContent: buildCollectionContentCell({
+        //     collectionContent: "https://raw.githubusercontent.com/Cosmodude/Nexton/main/collectionMetadata.json",
+        //     commonContent: " "
+        // }),
+        nftItemCode: await compile("NftItem"),
+        royaltyParams: {
+            royaltyFactor: Math.floor(Math.random() * 500), 
+            royaltyBase: 1000,
+            royaltyAddress: provider.sender().address as Address
+        }
+    }, await compile('NftCollection')));
 
+    console.log(provider.sender().address as Address)
     await nftCollection.sendDeploy(provider.sender(), toNano('0.05'));
-
+    console.log()
     await provider.waitForDeploy(nftCollection.address);
 
-    // run methods on `nftCollection`
+    const mint = await nftCollection.sendMintNft(provider.sender(),{
+        value: toNano("0.04"),
+        queryId: randomSeed,
+        amount: 14000000n,
+        itemIndex: 0,
+        itemOwnerAddress: myAddress,
+        itemContent: setItemContentCell({
+            name: "OnChain",
+            description: "Holds onchain metadata",
+            image: "",
+        })
+    })
+    console.log(`NFT Item deployed at https://testnet.tonviewer.com/${nftCollection.address}`);
 }
